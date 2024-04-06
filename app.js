2bc3acc9959147892355af6850805bd1aa9b0bf8
runohod <andrei.rumynthev1728@yandex.ru>
2024-04-06 19:15:56 +0300

    1

--- app.js
+++ app.js
@@ -3,8 +3,13 @@
  //const isProgrammer = true
 
 const resultElement = document.getElementById('result')
-const input1 = document.getElementById('input')
-const resultElement = document.getElementById('result')
-const resultElement = document.getElementById('result')
-const resultElement = document.getElementById('result')
-const resultElement = document.getElementById('result')
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const submitBtn = document.getElementById('submit')
+const plusBtn = document.getElementById('submit')
+const minusBtn = document.getElementById('submit')
+
+submitBtn.onclick = function () {
+    const sum = Number(input1.value) + Number(input2.value)
+    resultElement.textContent = sum 
+}
