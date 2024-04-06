c7f07e1e4cbc7db04f26f7335da6887c58050509
runohod <andrei.rumynthev1728@yandex.ru>
2024-04-06 19:22:27 +0300

    1

--- app.js
+++ app.js
@@ -6,10 +6,30 @@ const resultElement = document.getElementById('result')
 const input1 = document.getElementById('input1')
 const input2 = document.getElementById('input2')
 const submitBtn = document.getElementById('submit')
-const plusBtn = document.getElementById('submit')
-const minusBtn = document.getElementById('submit')
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+let action = '+'
+
+
+plusBtn.onclick = function () {
+    action = '+'
+}
+
+minusBtnBtn.onclick = function () {
+    action = '-'
+}
+
+
 
 submitBtn.onclick = function () {
+
+    if (action =='+') {
     const sum = Number(input1.value) + Number(input2.value)
     resultElement.textContent = sum 
+    }
+
+    if (action =='-') {
+        const sum = Number(input1.value) - Number(input2.value)
+        resultElement.textContent = sum 
+        }
 }
