b59650fc655ec3f8c79353caf3427769b480e886
runohod <andrei.rumynthev1728@yandex.ru>
2024-04-07 16:52:44 +0300

    1

--- theory.js
+++ theory.js
@@ -5,6 +5,32 @@ let c = 17
 
 c = c + a 
 
-
+c += a
+c -= a
+c *= a
 
 console.log(c)
+
+
+const age = 29 //number 
+const name = 'vladilen' //string 
+const isProgrammer = true //boolean 
+let x //undefined 
+
+// ======= приоритеты 
+
+const fullAge = 29 
+const birthYear = 1993 
+const currentYear = 2023 
+
+// > < <= >= 
+
+const isFullAge = currentYear - birthYear < fullAge // true 
+
+
+//некст тема 
+const num1 = 42 // число 
+const num2 = '42'  // строка
+
+console.log(num1 === num2) 
+//=== проверяет как значение так и тип данных 
