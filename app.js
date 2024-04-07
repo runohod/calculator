84b21518675af2f81ace5ec8b7a4fe80c722cf8d
runohod <andrei.rumynthev1728@yandex.ru>
2024-04-07 15:36:30 +0300

    1

--- app.js
+++ app.js
@@ -19,19 +19,25 @@ minusBtnBtn.onclick = function () {
     action = '-'
 }
 
-
+function printResult (result) {
+    if (result < 0) {
+        resultElement.style.color = 'red'
+    } else {
+        resultElement.style.color = 'green'
+    }
+}
 
 submitBtn.onclick = function () {
 
     if (action =='+') {
-    const sum = Number(input1.value) + Number(input2.value)
-    resultElement.textContent = sum 
+    const result = Number(input1.value) + Number(input2.value)
+    printResult(sum)
     }
 
-    if (action =='-') {
-        const sum = Number(input1.value) - Number(input2.value)
-        resultElement.textContent = sum 
-        }
+    else if (action =='-') {
+    const sum = Number(input1.value) - Number(input2.value)
+    printResult(sum)
+    }
     
 }
 
