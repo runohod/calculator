2e22ad25714501ecda421719ab518dd3475560ff
runohod <andrei.rumynthev1728@yandex.ru>
2024-04-07 15:44:08 +0300

    1

--- app.js
+++ app.js
@@ -27,17 +27,32 @@ function printResult (result) {
     }
 }
 
+
+function computeNumbersWithAction (inp1, inp2, actionSymbol) {
+const num1 = Number(inp1.value)
+const num2 = Number(inp2.value)
+if (actionSymbol == '+') { 
+    return num1 + num2 
+}
+else if (actionSymbol == '-') { 
+    return num1 - num2 
+}
+
+}
+
 submitBtn.onclick = function () {
+    const result = computeNumbersWithAction( input1, input2, action)
+    printResult(result)
 
-    if (action =='+') {
-    const result = Number(input1.value) + Number(input2.value)
-    printResult(sum)
-    }
+    //if (action =='+') {
+   // const result = Number(input1.value) + Number(input2.value)
+    //printResult(sum)
+    //}
 
-    else if (action =='-') {
-    const sum = Number(input1.value) - Number(input2.value)
-    printResult(sum)
-    }
+    //else if (action =='-') {
+   // const sum = Number(input1.value) - Number(input2.value)
+    //printResult(sum)
+    //}
     
 }
 
